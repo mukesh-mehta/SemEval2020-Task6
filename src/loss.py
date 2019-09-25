e1fb1857a508f98ed68516ad8c2baaf5733e60e7
mukesh-mehta <mukesh.iitr18@gmail.com>
2019-09-25 20:50:02 +0530

    added custom loss fns

---
+++ src/loss.py
@@ -0,0 +1,18 @@
+#https://stackoverflow.com/questions/53354176/how-to-use-f-score-as-error-function-to-train-neural-networks
+import torch
+import torch.nn.functional as F
+
+def f1_loss(predict, target):
+    loss = 0
+    lack_cls = target.sum(dim=0) == 0
+    if lack_cls.any():
+        loss += F.binary_cross_entropy_with_logits(
+            predict[:, lack_cls], target[:, lack_cls])
+    predict = torch.sigmoid(predict)
+    predict = torch.clamp(predict * (1-target), min=0.01) + predict * target
+    tp = predict * target
+    tp = tp.sum(dim=0)
+    precision = tp / (predict.sum(dim=0) + 1e-8)
+    recall = tp / (target.sum(dim=0) + 1e-8)
+    f1 = 2 * (precision * recall / (precision + recall + 1e-8))
+    return 1 - f1.mean() + loss
